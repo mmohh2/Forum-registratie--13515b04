49fc5094f34170da389176fa6023e662a947d0c6
Mo <moh19951@outlook.com>
2019-12-04 14:19:54 +0100

    Uitwerking van de exercise

---
+++ forum_registratie.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Registratie</title>
+    </head>
+    <body>
+        <h1>Forum registratie</h1>
+        <form action="registratie_handler.php" method="post" class="form">
+            <div class="form">
+                <input type="text" name="username" id="username">Username <br>
+                <input type="email" name="email" id="email">Email <br>
+                <input type="number" name="age" id="age">Age <br>
+                <input type="submit" value="Submit">
+            </div>
+        </form>
+<?php
+?>
+    </body>
+</html>
---
+++ registratie_handler.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Forum registratie</h1>
+<?php
+echo "Jouw username is " . $_POST['username'];
+echo "<br>";
+echo "Jouw email is " . $_POST['email'];
+echo "<br>";
+echo "Jouw leeftijd is " . $_POST['age'];
+?>
+</body>
+</html>
